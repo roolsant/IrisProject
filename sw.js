232b44e93730111e39cd189a3f28a6584e9de16c
Rodrigo de Oliveira Santos <rodrigosantos2007@gmail.com>
2025-12-27 12:26:02 -0300

    Otimizações de layout e Funcionalidades
    1. PT(Br) - Adicionando suporte PWA - Adicionadas as imagens Stickers - Alteração da mascote - Ajuste no layout do jogo - Melhorias de funcionalidade.
    
    1. EN(Us)
    - Adding PWA support
    -Sticker images added
    - Mascot updated
    - Game layout adjustments
    - Functionality improvements

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'caca-cores-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './img/titulo_game.png',
+  './img/lente_camera.png',
+  './img/mascote_intro.png',
+  './img/icon-192.png',
+  './img/icon-512.png',
+  './img/stickers/sticker_aquarela.png',
+  './img/stickers/sticker_arcoiris.png',
+  './img/stickers/sticker_cachorrinho.png',
+  './img/stickers/sticker_caminhao_bombeiro.png',
+  './img/stickers/sticker_estrela.png',
+  './img/stickers/sticker_flor.png',
+  './img/stickers/sticker_frutas.png',
+  './img/stickers/sticker_gatinho.png',
+  './img/stickers/sticker_giz.png',
+  './img/stickers/sticker_morango.png',
+  './img/stickers/sticker_polvo.png',
+  './img/stickers/sticker_porquinho.png',
+  './img/stickers/sticker_sapo.png',
+  './img/stickers/sticker_sol.png',
+  './img/stickers/sticker_sorvete.png',
+  './img/stickers/sticker_tigre.png'
+];
+
+// Instala o Service Worker e baixa os arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Serve os arquivos do cache quando estiver offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
